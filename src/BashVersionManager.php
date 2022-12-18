fabdc71cbce54f4bb5e9a7c25530962859bf1253
Björn Hempel <bjoern@hempel.li>
2022-12-18 18:47:53 +0100

    Add composer.json to install this library via PHP composer package.

---
+++ src/BashVersionManager.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ixno/php-naming-conventions project.
+ *
+ * (c) Björn Hempel <https://www.hempel.li/>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Ixnode\BashVersionManager;
+
+/**
+ * Class BashVersionManager
+ *
+ * @author Björn Hempel <bjoern@hempel.li>
+ * @version 0.1.0 (2022-12-18)
+ * @since 0.1.0 (2022-12-18) First version.
+ */
+class BashVersionManager
+{
+}
